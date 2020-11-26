aa1f13a66e58402547fd6680cfd85df565acaec2
Kumar vibhanshu <vibhanshumonty@gmail.com>
2020-11-26 20:55:11 +0530

    update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Scrap-Post-News-Article
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
@@ -6,15 +7,12 @@ import urllib.request
 import random
 import csv
 
-
 from wordpress_xmlrpc import Client, WordPressPost
 from wordpress_xmlrpc.compat import xmlrpc_client
 from wordpress_xmlrpc.methods import media, posts
 
 #wordpress login client 
 client = Client('https://yourworpresswebsite/xmlrpc.php','username','password')
-
-
 
 #function for downloading top imagesfro news article
 def downloader(image_url):
@@ -24,6 +22,7 @@ def downloader(image_url):
 
 #using newspaper library
 from newspaper import Article
+
 #for example ,we used https://www.bhaskar.com/
 latestnews = newspaper.build('http://target website/',
                               memoize_articles=False)
@@ -35,7 +34,6 @@ length=(latestnews.size())
 print (length)
 for i in range(0,2):
     print (i)
-
     first_article = latestnews.articles[i]
     first_article.download()
     first_article.parse()
@@ -49,7 +47,6 @@ for i in range(0,2):
     csv_name = str(file_name) +'.csv'
     f = csv.writer(open(csv_name,'w', encoding='utf-8'))
     f.writerow([title,image,text])
-
     filename = full_file_name
 
     # prepare metadata
@@ -61,18 +58,12 @@ for i in range(0,2):
     # read the binary file and let the XMLRPC library encode it into base64
     with open(filename, 'rb') as img:
             data['bits'] = xmlrpc_client.Binary(img.read())
-
     response = client.call(media.UploadFile(data))
-
     attachment_id = response['id']
-
-
     post = WordPressPost()
     post.title = title
     post.content = text
     post.post_status = 'publish'
     post.thumbnail = attachment_id
     post.id = client.call(posts.NewPost(post))
-
-
     print ('Success')
